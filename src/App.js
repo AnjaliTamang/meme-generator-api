eb52ff3743eda2347a9e2dd2832a6dfc80b9a5d4
Anjali Tamang <lopchananjali@gmail.com>
2024-11-20 14:08:14 +0545

    push to git

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
+import MemeGen from "./components/MemeGen";
+import "bootstrap/dist/css/bootstrap.css";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <MemeGen />
+    </>
+  );
+}
